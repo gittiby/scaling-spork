fa492eaf44190969b372f40dca56a5965630c52a
gittiby <fibbity3@gmail.com>
2018-05-18 15:19:00 -0700

    add rewire, readme

---
+++ README.md
@@ -0,0 +1,23 @@
+### a simple app for learning ws_next tools/frameworks:
+* graphql
+* couchbase
+* rewire
+
+uses the couchbase server sample database __travel-sample__
+
+### setup
+1. clone repo
+1. run `yarn`
+1. [setup couchbase server via docker](https://hub.docker.com/r/couchbase/server/)
+1. `npm run start`
+
+#### THE ws_next stack
+1. gRPC
+1. ksql
+1. graphql
+1. rewire
+1. couchdb
+1. kafka
+   * librdkafka?
+1. kubernetes
+1. elasticsearch?
--- src/server.ts
+++ src/server.ts
@@ -6,25 +6,23 @@ import {prettyLog} from './db/helpers';
 import CouchConnection from './db/dbutils';
 import {Airport} from './models/types';
 import * as helpers from './db/helpers';
-import {bucketName} from './server_configs';
+import {bucketName, couchDbURL} from './server_configs';
 
-// Initialize the app
 const app = express();
 
-// The GraphQL endpoint
 app.use('/graphql', bodyParser.json(), graphqlExpress({schema}));
 app.use('/graphiql', graphiqlExpress({ endpointURL: '/graphql' }));
 
-const cb: CouchConnection = new CouchConnection('http://localhost:8091', bucketName);
-// cb.testQuery();
+const cb: CouchConnection = new CouchConnection(couchDbURL, bucketName);
 
-cb.executeQuery(`SELECT * FROM \`${bucketName}\` WHERE type = 'airport' LIMIT 1`).then(json => {
-  let apjson = json[0][bucketName];
-  let ap: Airport = Airport.fromJson(apjson);
-  ap.saymyname();
-});
+cb.executeQuery(`SELECT * FROM \`${bucketName}\` WHERE type = 'airport' LIMIT 2`)
+  .then(json => json
+    .forEach(apJson => Airport
+      .fromJson(apJson[bucketName])
+      .saymyname()))
+  .catch(err => console.log(err));
 
-// Start server
+// start server
 const port: number = 4000
 app.listen(port, () => {
   console.log(`Visit http://localhost:${port}/graphiql to run queries.`);
